9eb4f6f3d82917e00cc879876dd43e84da6fcb02
devkroma <joejames1996@hotmail.com>
2017-07-15 19:30:32 +0100

    Initial Commit

---
+++ Database/mongo.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using GTANetworkServer;
+
+namespace kroma_cnr.Database
+{
+    public static class DatabaseClass
+    {
+        static public MongoClient CnRClient = new MongoClient("mongodb://localhost:27017");
+        static public IMongoDatabase CnRDatabase { get; set; }
+    }
+    class Mongo
+    {
+        public static void Main()
+        {
+            DatabaseClass.CnRDatabase = DatabaseClass.CnRClient.GetDatabase("kroma-cnr");
+            API.shared.consoleOutput("[MONGO] Starting database.");
+        }
+        public static async void CheckAccountExistsAsync(Client player)
+        {
+            var collection = DatabaseClass.CnRDatabase.GetCollection<PlayerAccount>("accounts");
+            var filter = Builders<PlayerAccount>.Filter.Eq("Name", player.name);
+            var result = await collection.Find(filter).ToListAsync();
+            if (result.Count() >= 1)
+            {
+                kroma_cnr.Main.OnAccountRegistered(player);
+            }
+            else
+            {
+                 kroma_cnr.Main.OnAccountNotRegistered(player);
+            }
+        }
+
+        public static void LoadAccount(string[] name)
+        {
+
+        }
+    }
+
+    public class PlayerAccount
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ kroma-cnr.cs
@@ -0,0 +1,38 @@
+﻿using GTANetworkServer;
+using System;
+
+namespace kroma_cnr
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            kroma_cnr.Database.Mongo.Main();
+            API.onResourceStart += MainOnResourceStart;
+            API.onPlayerConnected += MainOnPlayerConnected;
+        }
+
+        public void MainOnResourceStart()
+        {
+            API.consoleOutput("[KROMA-CNR] kroma's Cops & Robbers is starting.");
+        }
+
+        private void MainOnPlayerConnected(Client player)
+        {
+            API.consoleOutput("[KROMA-CNR] {0} has connected to the server.", player.name);
+            kroma_cnr.Database.Mongo.CheckAccountExistsAsync(player);
+        }
+
+        public static void OnAccountNotRegistered(Client player)
+        {
+            API.shared.sendChatMessageToPlayer(player, "This account isn't registered!");
+        }
+
+        public static void OnAccountRegistered(Client player)
+        {
+            API.shared.sendChatMessageToPlayer(player, "This account is already registered!");
+        }
+    }
+
+    //[Command("register")]
+}
